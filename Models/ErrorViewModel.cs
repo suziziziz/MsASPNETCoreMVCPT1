1f1ce52b090b971de9e65f22ebc9f6c8095a8f8c
Filype <lyperodrigues03@gmail.com>
2022-09-28 14:56:59 -0300

    Initial Commmit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MsASPNETCoreMVCPT1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
